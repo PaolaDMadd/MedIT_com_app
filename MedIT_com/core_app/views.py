d86c9909aac88c28da65cd26bcda4c0bcf53a7eb
PaolaDM <orchidea.preziosa@gmail.com>
2021-02-01 15:50:06 +0000

    add template folder and set urls

--- MedIT_com/core_app/views.py
+++ MedIT_com/core_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(req):
+    return HttpResponse("<h1>Hello! welcome index page!<h1>")
+
+def show(req):
+    return HttpResponse('<h3>welcome show page!</h3>')
+
+def login(req):
+    return HttpResponse("hello from login")
+
+def register(req):
+    return HttpResponse("hello from register")
