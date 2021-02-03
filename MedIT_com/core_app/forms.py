620d9fff440dd9a956e2e5b122b68dd504bea0b9
PaolaDM <orchidea.preziosa@gmail.com>
2021-02-03 14:18:12 +0000

    create user sign up form

--- MedIT_com/core_app/forms.py
+++ MedIT_com/core_app/forms.py
@@ -1,16 +1,12 @@
 from django import forms
-from django.forms import ModelForm, Textarea
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-class UserRegisterForm(UserCreationForm):
+class UserSignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=50)
     first_name = forms.CharField(max_length=50)
     last_name = forms.CharField(max_length=50)
-    email = forms.EmailField(max_length=50)
-    
-
 
     class Meta:
         model = User
-        fields = ['email', 'first_name', 'last_name', 'password1', 'password2']
-       
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
