42372465db16714a740d4c973e8fb26649637964
PaolaDM <orchidea.preziosa@gmail.com>
2021-02-03 14:37:33 +0000

    removed all unused files

--- MedIT_com/core_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import  views
-
-urlpatterns = [
-    path('', views.index, name='index'), 
-    path('login', views.login, name='login'), 
-    path('register', views.register, name='register'),
-    path('profile<int:id>', views.profile, name='profile') # to accept only numbers as id param
-]
