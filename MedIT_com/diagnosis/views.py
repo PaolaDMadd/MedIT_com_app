fdda1378c2db6082c2ca1fb5bbff2120bb83b5b4
Stylianos Thomas <stylianos.thomas@gmail.com>
2021-02-04 18:24:34 +0000

    History is now working. maybe fixed the prediction model getting stuck too

--- MedIT_com/diagnosis/views.py
+++ MedIT_com/diagnosis/views.py
@@ -16,23 +16,23 @@ def index(request):
             s3 = form.cleaned_data['symptom3']
             s4 = form.cleaned_data['symptom4']
             s5 = form.cleaned_data['symptom5']
-            result = NaiveBayes(s1,s2,s3,s4,s5)
+            result = NaiveBayes(s1, s2, s3, s4, s5)
             newEntry = DataEntry(symptom1=s1, symptom2=s2, symptom3=s3, symptom4=s4, symptom5=s5, diagnosis=result)
             newEntry.save()
-            print('Entry was create !!!!!!!')
-            return render(request, 'diagnosis/diagnosis.html', { 'form':form, 'result':result })
+            returnResult = result
+            result = ''
+            s1 = s2 = s3 = s4 = s5 = result = ''
+            print('looooool')
+            print('s1= ', s1,'s2 =', s2,'s3 =', s3,'s4 =', s4,'s5= ', s5,'result= ', result, returnResult)
+            return render(request, 'diagnosis/diagnosis.html', { 'form':form, 'result':returnResult })
     else:
         form = UserSymptoms()
     return render(request, 'diagnosis/diagnosis.html', { 'form':form })
 
 
 def history(request):
-    # userHistory = get_user_history()
-    # tomato = "tasty tomato"
-    # displayHistory = {'potato': tomato}
-    # for DataEntry in userHistory:
-    #     displayHistory.append(f'<li> {DataEntry} <li>')
-    return render(request, 'diagnosis/history.html')
+    userHistory = DataEntry.objects.all()
+    return render(request, 'diagnosis/history.html', { 'userHistory':userHistory })
 
 @login_required
 def profile(request):
